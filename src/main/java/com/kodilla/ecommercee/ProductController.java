cf2ffeb9a97abf0ddedcaa2284413d47b06c50c4
jacolec <jacwydro@gmail.com>
2022-09-30 20:42:46 +0200

    JDP221002-1

--- src/main/java/com/kodilla/ecommercee/ProductController.java
+++ src/main/java/com/kodilla/ecommercee/ProductController.java
@@ -1,4 +1,45 @@
 package com.kodilla.ecommercee;
 
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping//domain
 public class ProductController {
+
+    @GetMapping
+    public ResponseEntity<List<ProductDto>> getProducts() {
+        List<Product> products = new ArrayList<>();
+        //code connected with service, repository and mapper
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping(value = "{productId}")
+    public ResponseEntity<ProductDto> getProduct(@PathVariable Long productId) {
+        //code connected with service, repository and mapper
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> createProduct(@RequestBody ProductDto productDto) {
+        //code connected with service, repository and mapper
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping
+    public ResponseEntity<ProductDto> updateProduct(@RequestBody ProductDto productDto) {
+        //code connected with service, repository and mapper
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping(value = "{productId}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long productId) {
+        //code connected with service, repository and mapper
+        return ResponseEntity.ok().build();
+    }
+
+
 }
