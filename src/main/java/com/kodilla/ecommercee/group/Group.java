d527452dc4e81f692215c9aaf7da5174bd47d52d
lenczewskitom <lenczewski.tom@gmail.com>
2022-09-30 11:56:09 +0200

    Created GroupController, Group and GroupDto

---
+++ src/main/java/com/kodilla/ecommercee/group/Group.java
@@ -0,0 +1,7 @@
+package com.kodilla.ecommercee.group;
+
+public class Group {
+
+    private Long id;
+    private String name;
+}
--- src/main/java/com/kodilla/ecommercee/group/GroupController.java
+++ src/main/java/com/kodilla/ecommercee/group/GroupController.java
@@ -1,4 +1,36 @@
-package com.kodilla.ecommercee;
+package com.kodilla.ecommercee.group;
 
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/groups")
 public class GroupController {
+
+    @GetMapping
+    public List<GroupDto> getGroups() {
+        return new ArrayList<>();
+    }
+
+    @GetMapping(value = "{groupId}")
+    public GroupDto getGroup(@PathVariable Long groupId) {
+        return new GroupDto(1L, "test_group");
+    }
+
+    @PostMapping
+    public void createGroup() {
+
+    }
+
+    @PutMapping
+    GroupDto updateGroup() {
+        return new GroupDto(1L, "updated_test_group");
+    }
+
+    @DeleteMapping
+    public void deleteGroup(Long groupId) {
+
+    }
 }
