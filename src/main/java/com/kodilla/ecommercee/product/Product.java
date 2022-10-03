7d774a353b288c69e104f84e184d2ee58c732779
jacolec <jacwydro@gmail.com>
2022-10-03 17:41:38 +0200

    JDP221002-12

---
+++ src/main/java/com/kodilla/ecommercee/product/Product.java
@@ -0,0 +1,34 @@
+package com.kodilla.ecommercee.product;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Entity
+@Table(name = "PRODUCT")
+public class Product {
+
+    @Id
+    @GeneratedValue
+    @NotNull
+    @Column(name = "ID")
+    private Long id;
+
+    @Column(name = "NAME")
+    private String name;
+
+    @Column(name = "DESCRIPTION")
+    private String description;
+
+    @Column(name = "QUANTITY")
+    private int quantity;
+
+    @Column(name = "PRICE")
+    private double price;
+}
