ef4b1184e1e49fc48c6ca3f2e54760fa25634387
jacolec <jacwydro@gmail.com>
2022-10-06 16:35:45 +0200

    JDP221002-12

--- src/main/java/com/kodilla/ecommercee/product/Product.java
+++ src/main/java/com/kodilla/ecommercee/product/Product.java
@@ -1,5 +1,6 @@
 package com.kodilla.ecommercee.product;
 
+//import com.kodilla.ecommercee.group.Group;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -7,12 +8,20 @@ import lombok.NoArgsConstructor;
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 
-@AllArgsConstructor
+
 @NoArgsConstructor
 @Getter
 @Entity
 @Table(name = "PRODUCT")
 public class Product {
+
+    public Product(Long id, String name, String description, int quantity, double price) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.quantity = quantity;
+        this.price = price;
+    }
 
     @Id
     @GeneratedValue
@@ -31,4 +40,16 @@ public class Product {
 
     @Column(name = "PRICE")
     private double price;
+/*
+    private Group group;
+
+    @ManyToOne
+    @JoinColumn(name = "GROUP_ID")
+    public Group getGroup() {
+        return group;
+    }
+
+    public void setGroup(Group group) {
+        this.group = group;
+    } */
 }
