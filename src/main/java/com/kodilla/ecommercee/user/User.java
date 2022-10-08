9acbb80a1fa50a81ae8c448a177b6808b59c6e14
Karol Podsiadlo <karolekk131@gmail.com>
2022-10-08 11:15:26 +0200

    Adding @Column

--- src/main/java/com/kodilla/ecommercee/user/User.java
+++ src/main/java/com/kodilla/ecommercee/user/User.java
@@ -2,25 +2,34 @@ package com.kodilla.ecommercee.user;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 
 @Data
 @Entity
-@Table(name = "users")
+@Table(name = "USERS")
 public class User {
     @Id
+    @NotNull
     @GeneratedValue
+    @Column(name = "ID", unique = true)
     private Long id;
+    @Column(name = "LOGIN")
     private String login;
+    @Column(name = "PASSWORD")
     private String password;
+    @Column(name = "NAME")
     private String name;
+    @Column(name = "SURNAME")
     private String surname;
+    @Column(name = "ADDRESS")
     private String address;
+    @Column(name = "CITY")
     private String city;
+    @Column(name = "PHONE_NUMBER")
     private String phoneNumber;
+    @Column(name = "EMAIL")
     private String email;
+    @Column(name = "IS_BLOCK")
     private boolean isBlock;
 }
