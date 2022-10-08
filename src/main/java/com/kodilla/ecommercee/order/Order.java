7a7155e579f15e93d6ab006c6aa5393c16944509
lenczewskitom <lenczewski.tom@gmail.com>
2022-10-08 22:46:22 +0200

    Relacja encji Order z encja User

--- src/main/java/com/kodilla/ecommercee/order/Order.java
+++ src/main/java/com/kodilla/ecommercee/order/Order.java
@@ -4,10 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
@@ -34,4 +31,8 @@ public class Order {
 
     @Column(name = "SHIPPING_STATUS")
     private String shippingStatus;
+
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "USER_ID")
+    //private User user; komentarz do usunięcia po dodaniu encji User
 }
