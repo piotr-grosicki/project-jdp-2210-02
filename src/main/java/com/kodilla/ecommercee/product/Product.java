06097d61588a02c71aaf6718e9eca5bfddda17d8
Nessssscaf <99411361+Nessssscaf@users.noreply.github.com>
2022-10-14 10:34:57 +0200

    Update Product.java

--- src/main/java/com/kodilla/ecommercee/product/Product.java
+++ src/main/java/com/kodilla/ecommercee/product/Product.java
@@ -1,33 +1,22 @@
 package com.kodilla.ecommercee.product;
 
-import com.kodilla.ecommercee.cart.Cart;
 import com.kodilla.ecommercee.group.Group;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
-import java.util.List;
-
 
 @NoArgsConstructor
 @Getter
 @Entity
-@Table(name = "PRODUCT")
+@Table(name = "PRODUCTS")
 public class Product {
-
-    public Product(Long id, String name, String description, int quantity, double price) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.quantity = quantity;
-        this.price = price;
-    }
 
     @Id
     @GeneratedValue
     @NotNull
-    @Column(name = "PRODUCT_ID", unique = true)
+    @Column(name = "ID")
     private Long id;
 
     @Column(name = "NAME")
@@ -45,13 +34,4 @@ public class Product {
     @ManyToOne
     @JoinColumn(name = "GROUP_ID")
     private Group group;
-
-    @ManyToMany
-    @JoinTable(
-            name = "ProductsInCart",
-            joinColumns = @JoinColumn(name = "PRODUCT_ID"),
-            inverseJoinColumns = @JoinColumn(name = "CART_ID")
-    )
-    private List<Cart> carts;
 }
-
