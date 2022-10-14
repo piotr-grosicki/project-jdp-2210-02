cb9c97704ff95447fc93ea5631b73c0d2f3273f7
Nessssscaf <99411361+Nessssscaf@users.noreply.github.com>
2022-10-14 10:36:19 +0200

    Update Product.java

--- src/main/java/com/kodilla/ecommercee/product/Product.java
+++ src/main/java/com/kodilla/ecommercee/product/Product.java
@@ -1,17 +1,27 @@
 package com.kodilla.ecommercee.product;
 
 import com.kodilla.ecommercee.group.Group;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 
+
 @NoArgsConstructor
 @Getter
 @Entity
 @Table(name = "PRODUCT")
 public class Product {
+
+    public Product(Long id, String name, String description, int quantity, double price) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.quantity = quantity;
+        this.price = price;
+    }
 
     @Id
     @GeneratedValue
